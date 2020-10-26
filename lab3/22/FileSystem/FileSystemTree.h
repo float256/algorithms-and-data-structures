6bf290c0d48e8e70d9c130ef9ff5f428546b55bc
Nikita Cheremin <a1za2z307@gmail.com>
2020-10-26 14:52:27 +0300

    Added methods for moving through the file tree

--- lab3/22/FileSystem/FileSystemTree.h
+++ lab3/22/FileSystem/FileSystemTree.h
@@ -7,6 +7,8 @@
 
 #include <string>
 #include <variant>
+#include <stdexcept>
+#include <iostream>
 #include "FileSystemEntities.h"
 
 class FileSystemTree {
@@ -26,23 +28,81 @@ private:
     TreeNode* CurrentFolder = RootNode;
     int Count = 0;
 
+    void PrintOneNode(TreeNode* currNode, TreeNode* skippedNodes, int skippedNodesLength, std::string levelDesignation, int currLevel) {
+        if (currNode != nullptr) {
+            for (int i = 0; i < currLevel; ++i) {
+                std::cout << levelDesignation;
+            }
+            std::variant<TextFileInfo, FolderInfo> currNodeInfo = currNode->Data;
+            if (currNode->ElementType == FileSystemFolder) {
+                std::cout << std::get<FolderInfo>(currNodeInfo).Name << " (file)";
+            } else {
+                std::cout << std::get<TextFileInfo>(currNodeInfo).Name << " (text)";
+            }
+        }
+    }
+
+
 public:
     void AddFolder(std::string name) {
         TreeNode* previousChildNode = CurrentFolder -> FirstChildNode;
-        FolderInfo newFolderInfo = FolderInfo{
+        FolderInfo newFolderInfo = FolderInfo {
             .Name = name
         };
-        CurrentFolder -> FirstChildNode = new TreeNode{
+        CurrentFolder -> FirstChildNode = new TreeNode {
             .Data = newFolderInfo,
             .NextNeighbourNode = previousChildNode,
             .ParentNode = CurrentFolder,
+            .ElementType = FileSystemFolder
         };
+        ++Count;
+    }
+
+    void AddTextFile(std::string name, std::string text) {
+        TreeNode* previousChildNode = CurrentFolder -> FirstChildNode;
+        TextFileInfo newTextFileInfo = TextFileInfo {
+            .Name = name,
+            .Text = text
+        };
+        CurrentFolder -> FirstChildNode = new TreeNode {
+            .Data = newTextFileInfo,
+            .NextNeighbourNode = previousChildNode,
+            .ParentNode = CurrentFolder,
+            .ElementType = FileSystemFile
+        };
+        ++Count;
+    }
+
+    void CheckoutToChildNode(int childNodeOrder) {
+        TreeNode* currChildNode = CurrentFolder -> FirstChildNode;
+        if (childNodeOrder > Count - 1) {
+            throw std::out_of_range("Out of range when trying to get child node");
+        }
+        for (int i = 0; i < childNodeOrder; ++i) {
+            currChildNode = currChildNode -> NextNeighbourNode;
+        }
+        if (currChildNode -> ElementType != FileSystemFolder) {
+            throw std::invalid_argument("Node with specified order is not a folder");
+        }
+        CurrentFolder = currChildNode;
+    }
+
+    void CheckoutToParentNode() {
+        if (CurrentFolder -> ParentNode != nullptr) {
+            CurrentFolder = CurrentFolder -> ParentNode;
+        } else {
+            throw std::out_of_range("Current folder hasn't parent folder");
+        }
     }
 
     void AddExistingNode(TreeNode* node){
         TreeNode* previousChildNode = CurrentFolder -> FirstChildNode;
         node -> NextNeighbourNode = CurrentFolder -> NextNeighbourNode;
         CurrentFolder -> NextNeighbourNode = previousChildNode;
+    }
+
+    void PrintTree(TreeNode* skippedNodes, int skippedNodesLength, std::string levelDesignation = "//") {
+
     }
 };
 
